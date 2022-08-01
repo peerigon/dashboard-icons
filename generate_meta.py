ce27ef933144e09cef3911025f3649040a8571b6
Jonas Kugelmann <jonas.kugelmann@peerigon.com>
2022-08-01 15:19:09 +0200

    Added `package.json` and `AllIconNames.json`

---
+++ generate_meta.py
@@ -0,0 +1,26 @@
+import pathlib
+import json
+from pathlib import Path
+
+root = pathlib.Path(__file__).parent.resolve()
+json_path = root / "AllIconNames.json"
+
+
+pngs = sorted(Path("./png").glob("*.png"))
+svgs = sorted(Path("./svg").glob("*.svg"))
+
+def map_name(file):
+    return file.name
+
+output = {
+    "png": list(map(map_name, pngs)),
+    "svg": list(map(map_name, svgs))
+}
+
+# Write the new file
+with open(json_path, "w", encoding="UTF-8") as f:
+    json.dump(output, f, indent=2, sort_keys=True)
+    f.write("\n")
+
+print("Done!")
+print("Please commit the new AllIconNames.json file.")
